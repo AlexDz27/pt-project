2b6b2d62ff199cfd4e2f6d50d2de0d2add3792f0
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-03-26 11:28:42 +0300

    Feature: introduce search flow [WIP]

--- resources/js/components/routes/Search.vue
+++ resources/js/components/routes/Search.vue
@@ -6,28 +6,7 @@
       <div class="col-6">
         <h1 class="display-4 mb-7">Found locations</h1>
 
-        <div>
-          <button class="location-tab">
-            <div class="location-tab__photo-container">
-              <img class="location-tab__photo" width="300" height="200" src="https://a0.muscache.com/im/pictures/e855f284-bbe8-49b0-b557-eb11d1f42159.jpg?im_w=720" alt="Location">
-            </div>
-
-            <div class="location-tab__content">
-              <h4>Apartment "Temptation height"</h4>
-
-              <hr class="location-tab-separator location-tab__separator">
-
-              <div class="location-tab__amenities-container">
-                <span class="location-tab__amenity">
-                  1 bedroom
-                </span>
-                <span class="location-tab__amenity">
-                  Wifi
-                </span>
-              </div>
-            </div>
-          </button>
-
+        <div class="locations-list">
           <button class="location-tab">
             <div class="location-tab__photo-container">
               <span class="location-tab__no-photo">No photo</span>
@@ -77,14 +56,16 @@ export default {
     user: Object
   },
   mounted() {
-    this.initializeMap();
+    this.createList();
 
-    const locations = fetchLocations();
-
-    // TODO: continue...
+    // this.createMap();
   },
   methods: {
-    initializeMap() {
+    createList() {
+      console.log('123')
+    },
+
+    createMap() {
       const map = L.map('map').setView([51.505, -0.09], 13);
 
       L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYWxleGR6IiwiYSI6ImNrbW9zMDJ0dzI3cnAydm56MnNwZDF0aTkifQ.2HElYSbyH6nuS_5Zy4EZFg', {
@@ -96,19 +77,13 @@ export default {
         accessToken: 'pk.eyJ1IjoiYWxleGR6IiwiYSI6ImNrbW9zMDJ0dzI3cnAydm56MnNwZDF0aTkifQ.2HElYSbyH6nuS_5Zy4EZFg'
       }).addTo(map);
 
-      const marker = L.marker([51.5, -0.09]).addTo(map);
-      marker.bindPopup('<b>Hello world!</b><br>I am a popup.').openPopup();
+      const locations = fetchLocations();
 
-      var popup = L.popup();
+      for (let i = 0; i <= 10; i++) {
+        const location = locations[i];
 
-      function onMapClick(e) {
-        popup
-          .setLatLng(e.latlng)
-          .setContent('You clicked the map at ' + e.latlng.toString())
-          .openOn(map);
+        L.marker([location.latitude, location.longitude]).addTo(map);
       }
-
-      map.on('click', onMapClick);
     }
   }
 };
